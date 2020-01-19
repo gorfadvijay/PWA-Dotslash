904bd8918f3a231325535bb1e5fb51ab05f517b4
Vijay Gorfad <gorfadvijay1234@gmail.com>
2020-01-19 11:24:49 +0530

    PWA push

--- sw.js
+++ sw.js
@@ -1,13 +1,7 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
-];
+var filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js"];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener("hello-pwa", function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
@@ -16,7 +10,7 @@ self.addEventListener('install', function(e) {
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener("fetch", function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
